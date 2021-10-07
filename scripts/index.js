0886fa1b62d529b2a6767700faab6a8111cae9a0
Dmitry Khanin <dm.khanin@gmail.com>
2021-10-07 14:38:21 +0300

    feat: Rendering todos with innerHtml

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,18 @@ const todos = [
   'Разобраться в замыканиях',
   'Решить задачу на Codewars'
 ];
+
+const todosContainer = document.querySelector('.todos__list');
+
+const renderTodo = (taskName) => {
+  return `
+    <li class="todo-item">
+      <span class="todo-item__text">${taskName}</span>
+      <button class="todo-item__edit"></button>
+      <button class="todo-item__copy"></button>
+      <button class="todo-item__del"></button>
+    </li>
+  `;
+};
+
+todosContainer.innerHTML = todos.map(renderTodo).join('');
